c9a357dc933d2be44cfb3f8467f7ddedf3294cc3
samuele.malavasi <samuele.malavasi@gmail.com>
2020-06-27 12:31:17 +0200

    Made ThirdPartyRatesHelper singleton in order to instanciate it with application.properties once per run and handle third party service there

---
+++ src/main/java/com/exmachina/sam/currency/scheduledtasks/helpers/ThirdPartyRatesHelper.java
@@ -0,0 +1,41 @@
+package com.exmachina.sam.currency.scheduledtasks.helpers;
+
+import com.exmachina.sam.currency.scheduledtasks.pojos.ThirdPartyRates;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ThirdPartyRatesHelper {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String baseUrl = "https://min-api.cryptocompare.com/data/pricemulti?fsyms={source}&tsyms={comma_separated_targets}&api_key={key}";
+    private String apiKey;
+
+    private static ThirdPartyRatesHelper instance;
+
+    private ThirdPartyRatesHelper(String apiKey){
+        this.apiKey = apiKey;
+    }
+
+    public static ThirdPartyRatesHelper getInstance(String apiKey){
+        if ( instance == null) {
+            instance = new ThirdPartyRatesHelper(apiKey);
+        }
+        return instance;
+    }
+
+    public ThirdPartyRates getThirdPartyRatesBySourceAndTargetCurrency(String source , String[] targets){
+        Map<String, String> httpParams = new HashMap<String, String>();
+        httpParams.put("source" , source);
+        httpParams.put("comma_separated_targets" , String.join(",", targets));
+        httpParams.put("key" , apiKey);
+
+        return restTemplate.getForObject(baseUrl, ThirdPartyRates.class, httpParams);
+    }
+
+
+
+}
