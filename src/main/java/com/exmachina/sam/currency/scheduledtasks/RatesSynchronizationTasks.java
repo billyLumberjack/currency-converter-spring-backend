00a9411a8c87aef0d42a560c225df258850590f0
samuele.malavasi <samuele.malavasi@gmail.com>
2020-06-27 12:29:58 +0200

    Added properties and changed synch behaviour
    Added properties for third party currency service
    Added property for synch timeout
    Changed Synch to erase and re-populate table with updated data

--- src/main/java/com/exmachina/sam/currency/scheduledtasks/RatesSynchronizationTasks.java
+++ src/main/java/com/exmachina/sam/currency/scheduledtasks/RatesSynchronizationTasks.java
@@ -2,14 +2,17 @@ package com.exmachina.sam.currency.scheduledtasks;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Stream;
 
 import com.exmachina.sam.currency.entities.Rate;
 import com.exmachina.sam.currency.scheduledtasks.helpers.RatesMapper;
+import com.exmachina.sam.currency.scheduledtasks.helpers.ThirdPartyRatesHelper;
 import com.exmachina.sam.currency.scheduledtasks.pojos.ThirdPartyRates;
 import com.exmachina.sam.currency.services.interfaces.IRateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,13 +22,20 @@ import org.springframework.web.client.RestTemplate;
 public class RatesSynchronizationTasks {
 
 	@Autowired
+	private Environment env;
+	@Autowired
 	private IRateService rateService;
-	private final RestTemplate restTemplate = new RestTemplate();
-	private final String baseUrl = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD,EUR&api_key=12e663f7242e98c88435b6d8d939c276da860097834847d645f437c26935acd6";
 
-	@Scheduled(fixedRate = 5000)
+	@Scheduled(fixedRateString = "${scheduler.fixed.rate}")
 	public void ratesSynchronization() {
-		ThirdPartyRates updatedThirdPartyRates = restTemplate.getForObject(baseUrl, ThirdPartyRates.class);
+		ThirdPartyRatesHelper thirdPartyRatesHelper = ThirdPartyRatesHelper.getInstance(env.getProperty("thirdparty.converter.api.key"));
+		String sourceCurrencyToSynch = env.getProperty("thirdparty.converter.source.corrency");
+		String[] targetCurrencies = env.getProperty("thirdparty.converter.comma.separated.target.currencies").split(",");
+
+		ThirdPartyRates updatedThirdPartyRates = thirdPartyRatesHelper.getThirdPartyRatesBySourceAndTargetCurrency(
+				sourceCurrencyToSynch,
+				targetCurrencies
+		);
 		List<Rate> updatedRates = RatesMapper.mapThirdPartyRatesToRates(updatedThirdPartyRates);
 
 		rateService.deleteAll();
