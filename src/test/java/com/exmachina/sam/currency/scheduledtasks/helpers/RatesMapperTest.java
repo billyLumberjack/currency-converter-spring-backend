cf5610cbc4154cc949b7399d953119fca6011132
samuele.malavasi <samuele.malavasi@gmail.com>
2020-06-28 13:36:59 +0200

    Made RatesMapperTest working with tuples and correctly comparing lists

--- src/test/java/com/exmachina/sam/currency/scheduledtasks/helpers/RatesMapperTest.java
+++ src/test/java/com/exmachina/sam/currency/scheduledtasks/helpers/RatesMapperTest.java
@@ -5,11 +5,11 @@ import com.exmachina.sam.currency.scheduledtasks.pojos.ThirdPartyRates;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
 
 
 @SpringBootTest
@@ -18,31 +18,35 @@ public class RatesMapperTest {
     @Test
     public void mapThirdPartyRatesToRates(){
         ThirdPartyRates thirdPartyRates = createThirdPartyRatesWithFakes();
-        List<Rate> expectedMappedThirdPartyRates = new ArrayList<Rate>(){{
-            add(new Rate("source_currency_1" , "target_currency_1" , 3.14));
-            add(new Rate("source_currency_1" , "target_currency_2" , 1.59));
-            add(new Rate("source_currency_1" , "target_currency_3" , 2.65));
-            add(new Rate("source_currency_2" , "target_currency_1" , 3.14));
-            add(new Rate("source_currency_2" , "target_currency_2" , 1.59));
-            add(new Rate("source_currency_2" , "target_currency_3" , 2.65));
-        }};
         List<Rate> actualMappedThirdPartyRates = RatesMapper.mapThirdPartyRatesToRates(thirdPartyRates);
 
-        assertThat(actualMappedThirdPartyRates).containsAll(expectedMappedThirdPartyRates);
+        assertThat(actualMappedThirdPartyRates)
+                .extracting(
+                        rate -> rate.getSource(),
+                        rate -> rate.getDestination(),
+                        rate -> rate.getCoefficient())
+                .containsExactlyInAnyOrder(
+                        tuple("source_currency_1" , "destination_currency_1" , 3.14),
+                        tuple("source_currency_1" , "destination_currency_2" , 1.59),
+                        tuple("source_currency_1" , "destination_currency_3" , 2.65),
+                        tuple("source_currency_2" , "destination_currency_1" , 3.14),
+                        tuple("source_currency_2" , "destination_currency_2" , 1.59),
+                        tuple("source_currency_2" , "destination_currency_3" , 2.65)
+                );
     }
 
     private ThirdPartyRates createThirdPartyRatesWithFakes(){
 
-        LinkedHashMap<String, Double> targetsAndCoefficients = new LinkedHashMap<String, Double>()
+        LinkedHashMap<String, Double> destinationsAndCoefficients = new LinkedHashMap<String, Double>()
         {{
-            put("target_currency_1", 3.14);
-            put("target_currency_2", 1.59);
-            put("target_currency_3", 2.65);
+            put("destination_currency_1", 3.14);
+            put("destination_currency_2", 1.59);
+            put("destination_currency_3", 2.65);
         }};
 
         return new ThirdPartyRates(){{
-           put("source_currency_1", targetsAndCoefficients);
-           put("source_currency_2", targetsAndCoefficients);
+           put("source_currency_1", destinationsAndCoefficients);
+           put("source_currency_2", destinationsAndCoefficients);
         }};
 
     }
