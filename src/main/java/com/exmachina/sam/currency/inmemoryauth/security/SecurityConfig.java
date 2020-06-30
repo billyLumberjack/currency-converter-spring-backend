0be6b2563f8933935ac0cf82e47dfcf386adf464
samuele.malavasi <samuele.malavasi@gmail.com>
2020-06-30 11:10:30 +0200

    Created basic auth endpoint Added AuthBean to return on authentication Handled unauthorized cases in SecurityConfig Commented out explicit in-memory user creation and data

--- src/main/java/com/exmachina/sam/currency/inmemoryauth/security/SecurityConfig.java
+++ src/main/java/com/exmachina/sam/currency/inmemoryauth/security/SecurityConfig.java
@@ -1,52 +1,74 @@
 package com.exmachina.sam.currency.inmemoryauth.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private Environment env;
 
+    @Autowired
+    protected ObjectMapper objectMapper;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        AuthenticationEntryPoint authenticationEntryPoint = (request, response, e) -> {
+
+            AuthenticationBean authenticationBean = new AuthenticationBean(e.getMessage());
+            String authBeanJson = objectMapper.writeValueAsString(authenticationBean);
+
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(authBeanJson);
+        };
+
         http
-                .csrf().disable()
+                .csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/convert/**").hasRole(UserRoles.STRING_ADMIN.toString())
                 .antMatchers("/currencies/**").hasRole(UserRoles.STRING_ADMIN.toString())
                 .antMatchers("/rate/**").hasRole(UserRoles.STRING_ADMIN.toString())
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .httpBasic()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        String adminUsername = env.getProperty("spring.security.admin.username");
-        String adminPassword = env.getProperty("spring.security.admin.password");
-
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(adminUsername)
-                .password(encoder.encode(adminPassword))
-                .roles(UserRoles.STRING_ADMIN.toString());
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
     }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        String adminUsername = env.getProperty("spring.security.admin.username");
+//        String adminPassword = env.getProperty("spring.security.admin.password");
+//
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(adminUsername)
+//                .password(encoder.encode(adminPassword))
+//                .roles(UserRoles.STRING_ADMIN.toString());
+//    }
 }
