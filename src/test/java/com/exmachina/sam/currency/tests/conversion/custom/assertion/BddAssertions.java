9d2e1c4c44aff954595a6018c6de391bf6983745
samuele.malavasi <samuele.malavasi@gmail.com>
2020-07-03 00:38:46 +0200

    Added assertion generator plugin and generated assertions

---
+++ src/test/java/com/exmachina/sam/currency/tests/conversion/custom/assertion/BddAssertions.java
@@ -0,0 +1,26 @@
+package com.exmachina.sam.currency.tests.conversion.custom.assertion;
+
+/**
+ * Entry point for BDD assertions of different data types.
+ */
+@javax.annotation.Generated(value="assertj-assertions-generator")
+public class BddAssertions {
+
+  /**
+   * Creates a new instance of <code>{@link CurrenciesConversionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public static CurrenciesConversionAssert then(com.exmachina.sam.currency.conversion.CurrenciesConversion actual) {
+    return new CurrenciesConversionAssert(actual);
+  }
+
+  /**
+   * Creates a new <code>{@link BddAssertions}</code>.
+   */
+  protected BddAssertions() {
+    // empty
+  }
+}
---
+++ src/test/java/com/exmachina/sam/currency/tests/conversion/custom/assertion/SoftAssertions.java
@@ -0,0 +1,20 @@
+package com.exmachina.sam.currency.tests.conversion.custom.assertion;
+
+/**
+ * Entry point for soft assertions of different data types.
+ */
+@javax.annotation.Generated(value="assertj-assertions-generator")
+public class SoftAssertions extends org.assertj.core.api.SoftAssertions {
+
+  /**
+   * Creates a new "soft" instance of <code>{@link CurrenciesConversionAssert}</code>.
+   *
+   * @param actual the actual value.
+   * @return the created "soft" assertion object.
+   */
+  @org.assertj.core.util.CheckReturnValue
+  public CurrenciesConversionAssert assertThat(com.exmachina.sam.currency.conversion.CurrenciesConversion actual) {
+    return proxy(CurrenciesConversionAssert.class, com.exmachina.sam.currency.conversion.CurrenciesConversion.class, actual);
+  }
+
+}
